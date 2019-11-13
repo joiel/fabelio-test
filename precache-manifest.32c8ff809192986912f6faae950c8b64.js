eacbaed6c016f383ab5790e2b4fff3d640781381
Joko Santoso <developer.laper@gmail.com>
2019-11-13 21:58:51 +0700

    Updates

---
+++ precache-manifest.32c8ff809192986912f6faae950c8b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf11e14ed0ac9bd70f8163355f5463c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d1397863172a1e01906",
+    "url": "/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "6e64d2ba10155cb5237d",
+    "url": "/static/css/main.8c947623.chunk.css"
+  },
+  {
+    "revision": "5d1397863172a1e01906",
+    "url": "/static/js/2.99dacc29.chunk.js"
+  },
+  {
+    "revision": "6e64d2ba10155cb5237d",
+    "url": "/static/js/main.2f5cc6b6.chunk.js"
+  },
+  {
+    "revision": "5d498cff3342942db387",
+    "url": "/static/js/runtime-main.07b258b6.js"
+  }
+]);
